2ccc3417ee88d73e92ac01526be0185919afd413
moihm96 <moihermol@gmail.com>
2019-04-11 11:54:07 +0100

    cambios

--- App.js
+++ App.js
@@ -1,31 +1,12 @@
 import React, { Component } from 'react';
-import * as firebase from "firebase";
 import Routes from './services/Router';
-import {createStore,applyMiddleware} from "redux";
-import ReduxThunk from "redux-thunk"
-import {Provider} from "react-redux"
-import reducers from "./Reducers"
+
 class App extends Component {
-    componentWillMount() {
-        const config = {
-            apiKey: "AIzaSyBOI8LwWP-1VRLNMicdecjNanQIIQmK0EU",
-            authDomain: "react-native-a2f5d.firebaseapp.com",
-            databaseURL: "https://react-native-a2f5d.firebaseio.com",
-            projectId: "react-native-a2f5d",
-            storageBucket: "react-native-a2f5d.appspot.com",
-            messagingSenderId: "41761192519"
-        };
-        firebase.initializeApp(config);
-        console.log(firebase)
-}
 
     render() {
-        const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
 
         return (
-            <Provider store={store}>
-                <Routes/>
-            </Provider>
+            <Routes/>
         )
     }
 }
