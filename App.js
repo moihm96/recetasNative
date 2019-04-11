fcfc80096c3f2b31a0ac15c6140a734bea590940
moihm96 <moihermol@gmail.com>
2019-04-11 12:09:40 +0100

    firebase inicializado

--- App.js
+++ App.js
@@ -1,7 +1,20 @@
 import React, { Component } from 'react';
 import Routes from './services/Router';
-
+import * as firebase from "firebase"
 class App extends Component {
+    componentWillMount() {
+        const config={
+            apiKey: "AIzaSyBOI8LwWP-1VRLNMicdecjNanQIIQmK0EU",
+            authDomain: "react-native-a2f5d.firebaseapp.com",
+            databaseURL: "https://react-native-a2f5d.firebaseio.com",
+            projectId: "react-native-a2f5d",
+            storageBucket: "react-native-a2f5d.appspot.com",
+            messagingSenderId: "41761192519"
+        }
+        firebase.initializeApp(config);
+        console.log(firebase)
+    }
+
     render() {
         return (
             <Routes />
